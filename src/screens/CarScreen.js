3975a1e2ea1392da50b2f502f97eb42660ac08cb
ITTECH10 <ittech.a1development@gmail.com>
2021-08-12 09:28:32 +0200

    car screen fix

--- src/screens/CarScreen.js
+++ src/screens/CarScreen.js
@@ -6,13 +6,13 @@ import { Typography } from '@material-ui/core'
 import Loader from '../utils/Loader'
 
 const CarScreen = () => {
-    const {user, selectedUser, loading} = useData()
+    const { user, selectedUser, loading } = useData()
     return (
         !loading ?
-        <React.Fragment>
-            {selectedUser._id ? <CarTable /> : <Typography variant="h4">No customer selected.</Typography>}
-            {user.role === 'admin' && <UploadCarData />}
-        </React.Fragment> : <Loader />
+            <React.Fragment>
+                {selectedUser._id && user.role === 'admin' ? <CarTable /> : <Typography variant="h4">No customer selected.</Typography>}
+                {user.role === 'admin' && <UploadCarData />}
+            </React.Fragment> : <Loader />
     )
 }
 
