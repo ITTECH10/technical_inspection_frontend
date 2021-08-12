4757bd5308fba45fb0953f0a21b4515028d69b0e
ITTECH10 <ittech.a1development@gmail.com>
2021-08-12 10:01:44 +0200

    added logout loading

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useCallback} from 'react'
+import React, { useContext, useState, useCallback } from 'react'
 import axios from 'axios'
 
 const DataContext = React.createContext()
@@ -7,7 +7,7 @@ export const useData = () => {
     return useContext(DataContext)
 }
 
-const DataContextProvider = ({children}) => {
+const DataContextProvider = ({ children }) => {
     // GENERAL
     const [authenticated, setAuthenticated] = useState(false)
     const [loading, setLoading] = useState(false)
@@ -17,11 +17,11 @@ const DataContextProvider = ({children}) => {
     const [user, setUser] = useState({})
     const [users, setUsers] = useState([])
     const [selectedUser, setSelectedUser] = useState({})
-    
+
     // VEHICLES
     const [myVehicles, setMyVehicles] = useState([])
     const [selectedCar, setSelectedCar] = useState([])
-    
+
     // INSURANCES
     const [insurances, setInsurances] = useState([])
     const [selectedCarInsurance, setSelectedCarInsurance] = useState({})
@@ -31,96 +31,100 @@ const DataContextProvider = ({children}) => {
     const [selectedCarBank, setSelectedCarBank] = useState({})
 
     const logout = useCallback((history) => {
+        setAppLoading(true)
         localStorage.removeItem('token')
         setAuthenticated(false)
         delete axios.defaults.headers.common['Authorization']
-        history.push('/')
-        history.go(0)
+        setTimeout(() => {
+            setAppLoading(false)
+            history.push('/')
+            // history.go(0) // optional
+        }, 2000)
     }, [])
 
     const getAllUsers = useCallback(() => {
         axios('/users').then(res => {
             setUsers(res.data.users)
         })
-        .catch(err => {
-            console.log(err.response)
-        })
+            .catch(err => {
+                console.log(err.response)
+            })
     }, [])
 
     const getUserData = useCallback(() => {
         setAppLoading(true)
         axios('/users/me').then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setUser(res.data.user)
                 setAppLoading(false)
             }
         })
-        .catch(err => {
-            setAppLoading(false)
-            console.log(err.response)
-        })
+            .catch(err => {
+                setAppLoading(false)
+                console.log(err.response)
+            })
     }, [])
 
     const getSelectedUser = useCallback((id) => {
         setLoading(true)
-        
+
         axios(`/users/${id}`).then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setLoading(false)
                 setSelectedUser(res.data.user)
             }
         })
-        .catch(err => {
-            setLoading(false)
-            console.log(err.response)
-        })
+            .catch(err => {
+                setLoading(false)
+                console.log(err.response)
+            })
     }, [])
 
-    const getSelectedCar = useCallback((id) => {        
+    const getSelectedCar = useCallback((id) => {
         axios(`/cars/car/${id}`).then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setSelectedCar(res.data.vehicle)
             }
         })
-        .catch(err => {
-            console.log(err.response)
-        })
+            .catch(err => {
+                console.log(err.response)
+            })
     }, [])
 
     const getUserVehicles = useCallback((id) => {
         // setLoading(true)
-        
+
         axios(`/cars/${id}`).then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setMyVehicles(res.data.userVehicles)
             }
         })
-        .catch(err => {
-            // setLoading(false)
-            console.log(err.response)
-        })
+            .catch(err => {
+                // setLoading(false)
+                console.log(err.response)
+            })
     }, [])
 
     const getInsurances = useCallback(() => {
         axios.get('/insuranceHouse').then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setInsurances(res.data.insurances)
             }
         })
-        .catch(err => {
-            console.log(err.response)
-        })
+            .catch(err => {
+                console.log(err.response)
+            })
     }, [])
 
     const getBanks = useCallback(() => {
         axios.get('/payment').then(res => {
-            if(res.status === 200) {
+            if (res.status === 200) {
                 setBanks(res.data.banks)
             }
         })
-        .catch(err => {
-            console.log(err.response)
-        })
+            .catch(err => {
+                console.log(err.response)
+            })
     }, [])
 
     const value = {
