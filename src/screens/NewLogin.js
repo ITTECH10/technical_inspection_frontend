4112f88cb789e5355475c0b44690cef43b2f70c1
ITTECH10 <ittech.a1development@gmail.com>
2022-03-10 13:39:52 +0100

    new login page made responsive

--- src/screens/NewLogin.js
+++ src/screens/NewLogin.js
@@ -20,7 +20,6 @@ const NewLogin = ({ history, t }) => {
         email: '',
         password: ''
     })
-    const [errors, setErrors] = useState({})
     const { setAuthenticated, setAppLoading, appLoading, setSelectedIndex, setGeneralAlertOptions } = useData()
     const [disableSubmiting, setDisableSubmiting] = useState(false)
 
@@ -35,6 +34,7 @@ const NewLogin = ({ history, t }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setDisableSubmiting(true)
 
         const data = { ...fields }
         axios.post('/users/login', data).then(res => {
@@ -69,108 +69,110 @@ const NewLogin = ({ history, t }) => {
                 <link rel="stylesheet" href="style.css" />
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
             </Helmet>
-            <Box className="wrapper">
-                {matches &&
-                    <Box component="header" className="header-login">
-                        <Box className="logo">
-                            <img src="logo.svg" />
-                        </Box>
-                        <Box className="separator" />
-                        <Box>
-                            <Box className="info">
-                                <Box className="street">
-                                    <Icon>
-                                        <PlaceIcon color="error" />
-                                    </Icon>
-                                    <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="https://goo.gl/maps/th6gNdvVJ6ihiUA3A">Geltinger Str. 23, 85652 Pliening</a></Typography>
-                                </Box>
-                                <Box className="telephone">
-                                    <Icon>
-                                        <PhoneIcon color="error" />
-                                    </Icon>
-                                    <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="+4981212243822">+49 8121 22 43 8 – 22</a></Typography>
-                                </Box>
-                                <Box className="mail">
-                                    <Icon>
-                                        <MailIcon color="error" />
-                                    </Icon>
-                                    <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="mailto:info@se-carmanagement.de">info@se-carmanagement.de</a></Typography>
+            <Box sx={{ position: 'relative', height: '100vh', width: '100vw' }}>
+                <Box className="wrapper">
+                    {matches &&
+                        <Box component="header" className="header-login">
+                            <Box className="logo">
+                                <img src="logo.svg" />
+                            </Box>
+                            <Box className="separator" />
+                            <Box>
+                                <Box className="info">
+                                    <Box className="street">
+                                        <Icon>
+                                            <PlaceIcon color="error" />
+                                        </Icon>
+                                        <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="https://goo.gl/maps/th6gNdvVJ6ihiUA3A">Geltinger Str. 23, 85652 Pliening</a></Typography>
+                                    </Box>
+                                    <Box className="telephone">
+                                        <Icon>
+                                            <PhoneIcon color="error" />
+                                        </Icon>
+                                        <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="+4981212243822">+49 8121 22 43 8 – 22</a></Typography>
+                                    </Box>
+                                    <Box className="mail">
+                                        <Icon>
+                                            <MailIcon color="error" />
+                                        </Icon>
+                                        <Typography style={{ fontFamily: 'Eurostile, sans-serif' }}><a href="mailto:info@se-carmanagement.de">info@se-carmanagement.de</a></Typography>
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
-                    </Box>}
-                <Box sx={{ px: matches ? 0 : '20px' }} component="main">
-                    <Box className="screen">
-                        <Box className="screen__content">
-                            <Box className="logo-button">
-                                <div />
-                            </Box>
-                            <Box component="form" onSubmit={handleSubmit} className="login">
-                                <Box className="login__heading"><h2>Fuhrparkmanager</h2></Box>
-                                <Box className="login__subheading">
-                                    Haben Sie noch keinen Zugang, dann setzen Sie sich bitte mit uns in Verbindung.
+                        </Box>}
+                    <Box sx={{ px: matches ? 0 : '20px' }} component="main">
+                        <Box className="screen">
+                            <Box className="screen__content">
+                                <Box className="logo-button">
+                                    <div />
                                 </Box>
-                                <Box className="login__field">
-                                    <i className="login__icon fas fa-user"></i>
-                                    <TextField
-                                        name="email"
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        size="small"
-                                        type="text"
-                                        style={{
-                                            backgroundColor: '#fff',
-                                            borderRadius: 5,
-                                            fontWeight: 700,
-                                            fontSize: 'medium',
-                                            fontFamily: 'Eurostile, sans-serif'
-                                        }}
-                                        fullWidth
-                                        placeholder="Email"
-                                    />
-                                </Box>
-                                <Box className="login__field">
-                                    <i className="login__icon fas fa-lock"></i>
-                                    <TextField
-                                        name="password"
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        size="small"
-                                        type="password"
-                                        style={{
-                                            backgroundColor: '#fff',
-                                            borderRadius: 5,
-                                            fontWeight: 700,
-                                            fontSize: 'medium',
-                                            fontFamily: 'Eurostile, sans-serif'
-                                        }}
-                                        fullWidth
-                                        placeholder="Passwort"
-                                    />
-                                </Box>
-                                <button className="button login__submit" type="submit">
-                                    <Icon style={{ '&:hover': { boxShadow: 'none !important' }, padding: '5px' }}>
-                                        <ExitToAppIcon style={{ color: '#fff' }} />
-                                    </Icon>
-                                    <span className="button__text">Anmelden</span>
-                                </button>
-                                {/* <Box className="forgot_password">
+                                <Box component="form" onSubmit={handleSubmit} className="login">
+                                    <Box className="login__heading"><h2>Fuhrparkmanager</h2></Box>
+                                    <Box className="login__subheading">
+                                        Haben Sie noch keinen Zugang, dann setzen Sie sich bitte mit uns in Verbindung.
+                                    </Box>
+                                    <Box className="login__field">
+                                        <i className="login__icon fas fa-user"></i>
+                                        <TextField
+                                            name="email"
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            size="small"
+                                            type="text"
+                                            style={{
+                                                backgroundColor: '#fff',
+                                                borderRadius: 5,
+                                                fontWeight: 700,
+                                                fontSize: 'medium',
+                                                fontFamily: 'Eurostile, sans-serif'
+                                            }}
+                                            fullWidth
+                                            placeholder="Email"
+                                        />
+                                    </Box>
+                                    <Box className="login__field">
+                                        <i className="login__icon fas fa-lock"></i>
+                                        <TextField
+                                            name="password"
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            size="small"
+                                            type="password"
+                                            style={{
+                                                backgroundColor: '#fff',
+                                                borderRadius: 5,
+                                                fontWeight: 700,
+                                                fontSize: 'medium',
+                                                fontFamily: 'Eurostile, sans-serif'
+                                            }}
+                                            fullWidth
+                                            placeholder="Passwort"
+                                        />
+                                    </Box>
+                                    <button className="button login__submit" type="submit">
+                                        <Icon style={{ '&:hover': { boxShadow: 'none !important' }, padding: '5px' }}>
+                                            <ExitToAppIcon style={{ color: '#fff' }} />
+                                        </Icon>
+                                        <span className="button__text">Anmelden</span>
+                                    </button>
+                                    {/* <Box className="forgot_password">
                                     <a href="">Passwort vergessen</a>
                                 </Box> */}
-                                <ForgotPasswordForm onDisableLoginForm={setDisableSubmiting} />
+                                    <ForgotPasswordForm onDisableLoginForm={setDisableSubmiting} />
+                                </Box>
                             </Box>
                         </Box>
                     </Box>
-                </Box>
-                <Box component="footer" sx={{ fontSize: matches ? '1rem' : '.5rem' }}>
-                    <Box className="copyright">
-                        <Box className="column">&copy; Copyright SE-Carmanagement 2021 - 2022 | Anschaffung, Fuhrparkmanagement und
-                            Schadenservice</Box>
-                    </Box>
-                    <Box className="separator"></Box>
-                    <Box className="links">
-                        <Box className="column">
-                            <a href="http://sec.togemaxx.de/impressum/">Impressum</a> <span>|</span> <a href="http://sec.togemaxx.de/datenschutzerklaerun">Datenschutz</a></Box>
+                    <Box component="footer" sx={{ fontSize: matches ? '1rem' : '.5rem' }}>
+                        <Box className="copyright">
+                            <Box className="column">&copy; Copyright SE-Carmanagement 2021 - 2022 | Anschaffung, Fuhrparkmanagement und
+                                Schadenservice</Box>
+                        </Box>
+                        <Box className="separator"></Box>
+                        <Box className="links">
+                            <Box className="column">
+                                <a href="http://sec.togemaxx.de/impressum/">Impressum</a> <span>|</span> <a href="http://sec.togemaxx.de/datenschutzerklaerun">Datenschutz</a></Box>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
